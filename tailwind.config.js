bacd3a4ec5e7ef611d17c2ecfb9cad428cce53cc
k-kbk <kkbk-@naver.com>
2022-12-05 20:46:48 +0900

    chore: tailwindcss 설정

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        88: '22rem',
+        104: '26rem',
+        112: '28rem',
+        128: '32rem',
+      },
+    },
+  },
+  plugins: [],
+};
