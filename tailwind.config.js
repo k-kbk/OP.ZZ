8f6aa6f679b9a6a42bdd48154980df013ca080a1
k-kbk <kkbk-@naver.com>
2022-12-06 17:20:44 +0900

    chore: main 컬러 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      main: 'rgb(45, 45, 45)',
+    },
     extend: {
       spacing: {
         88: '22rem',
