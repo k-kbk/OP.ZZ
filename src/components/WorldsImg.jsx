9e87ad303d81c7c00193d19e4ee7cddb47d70662
k-kbk <kkbk-@naver.com>
2022-12-06 17:42:52 +0900

    design: 투명도 수정

--- src/components/WorldsImg.jsx
+++ src/components/WorldsImg.jsx
@@ -3,7 +3,7 @@ import Logo from '../assets/worlds.png';
 
 const WorldsImg = (props) => {
   return (
-    <div className='max-w-[15rem] max-h-[15rem] w-1/2 sm:w-full mt-8 opacity-80'>
+    <div className='max-w-[15rem] max-h-[15rem] w-1/2 sm:w-full mt-8 opacity-70'>
       <img src={Logo} alt='worlds' />
     </div>
   );
