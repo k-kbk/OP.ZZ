a8be1033f4f2faad5cef762d0acec93c087d33a8
k-kbk <kkbk-@naver.com>
2022-12-10 14:07:49 +0900

    feat: 더 보기 기능 구현

--- src/components/Match/MatchHistory.jsx
+++ src/components/Match/MatchHistory.jsx
@@ -1,80 +1,147 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import axios from 'axios';
+import { Ring } from '@uiball/loaders';
+import Axios from 'axios';
 import Match from './Match';
 
-const MatchHistory = () => {
-  const { state: summoner } = useLocation();
-  const [matchIds, setMatchIds] = useState(null);
-  const [matchData, setMatchData] = useState(null);
+const axios = Axios.create();
+axios.interceptors.response.use(undefined, (error) => {
+  const config = error.config;
+  config.retryCount = isNaN(config.retryCount) ? 0 : config.retryCount;
+  config.retryCount += 1;
+  if (config.retryCount > 2) {
+    return Promise.reject(error);
+  }
+  return axios.request(config);
+});
+
+const MatchHistory = (props) => {
+  const summoner = props.summoner;
+  const [loading, setLoading] = useState(true);
+  const [moreLoading, setMoreLoading] = useState(false);
+  const [start, setStart] = useState(0);
+  const [matchIds, setMatchIds] = useState([]);
+  const [matchData, setMatchData] = useState([]);
   const API_KEY = process.env.REACT_APP_API_KEY;
 
+  const getMatchIds = async () => {
+    await axios
+      .get(
+        `https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${summoner.puuid}/ids`,
+        {
+          params: {
+            start,
+            count: 10,
+            api_key: API_KEY,
+          },
+          timeout: 1000,
+        }
+      )
+      .then((res) => {
+        setMatchIds(res.data);
+        setStart((prev) => prev + 10);
+        return;
+      })
+      .catch((err) => {
+        alert('잠시 후 다시 시도해주세요.');
+        return setLoading(false);
+      });
+  };
+
+  const getMatchData = (matchId) => {
+    return axios.get(
+      `https://asia.api.riotgames.com/lol/match/v5/matches/${matchId}`,
+      {
+        params: {
+          api_key: API_KEY,
+        },
+        timeout: 1000,
+      }
+    );
+  };
+
+  const moreLoadingHandler = () => {
+    setMoreLoading(true);
+    getMatchIds();
+    return;
+  };
+
   useEffect(() => {
-    const getMatchId = async () => {
-      await axios
-        .get(
-          `https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${summoner.puuid}/ids`,
-          {
-            params: {
-              start: 0,
-              count: 10,
-              api_key: API_KEY,
-            },
-          }
-        )
-        .then((res) => {
-          setMatchIds(res.data);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
+    setLoading(true);
+    getMatchIds();
+    return () => {
+      console.log('clean up');
+      setStart(0);
     };
-    getMatchId();
   }, [summoner]);
 
   useEffect(() => {
-    if (matchIds !== null) {
-      const getMatchData = (matchId) => {
-        return axios.get(
-          `https://asia.api.riotgames.com/lol/match/v5/matches/${matchId}`,
-          {
-            params: {
-              api_key: API_KEY,
-            },
-          }
-        );
-      };
-
+    if (matchIds.length !== 0) {
+      console.log('here');
       (async function () {
-        await axios
-          .all(
-            matchIds.map((matchId) => {
-              return getMatchData(matchId);
-            })
-          )
-          .then(
-            axios.spread((...res) => {
-              const correctData = res.filter(
-                (item) => item.data.info.gameCreation !== 0
-              );
-              setMatchData(correctData.map((item) => item.data.info));
-            })
-          );
+        await Promise.all(
+          matchIds.map((matchId) => {
+            return getMatchData(matchId);
+          })
+        )
+          .then((...res) => {
+            const correctData = res[0].filter(
+              (item) => item.data.info.gameCreation !== 0
+            );
+            const newMatchData = correctData.map((item) => item.data.info);
+            console.log('get match data');
+            setMatchData((prev) => [...prev, ...newMatchData]);
+            setLoading(false);
+            setMoreLoading(false);
+            console.log('change');
+            return;
+          })
+          .catch((err) => {
+            console.log(err);
+            setLoading(false);
+            setMoreLoading(false);
+            console.log(loading);
+            return;
+          });
       })();
     }
     return;
   }, [matchIds]);
 
   return (
-    <div className='w-full'>
+    <div className='max-w-4xl w-4/5 sm:w-full mb-3'>
       <div className='mb-4 border-b border-myGray'>
-        <p className='font-bold text-lg text-myIndigo py-2'>전체</p>
+        <p className='font-bold text-base sm:text-lg text-myIndigo pb-2'>
+          전체
+        </p>
       </div>
-      <ul className='w-full flex flex-col items-center gap-3'>
-        {matchData &&
-          matchData.map((match) => <Match key={match.gameId} data={match} />)}
-      </ul>
+      {loading ? (
+        <div className='h-52 flex justify-center items-center'>
+          <Ring size={50} lineWeight={4} speed={2} color='black' />
+        </div>
+      ) : (
+        <>
+          <ul className='w-full flex flex-col items-center gap-3'>
+            {matchData.length !== 0 &&
+              matchData.map((match) => (
+                <Match key={summoner.puuid + match.gameId} data={match} />
+              ))}
+          </ul>
+          <div className='w-full h-10 flex justify-center mt-3'>
+            {moreLoading ? (
+              <div className='w-full h-full flex justify-center items-center'>
+                <Ring size={20} lineWeight={4} speed={2} color='black' />
+              </div>
+            ) : (
+              <button
+                onClick={moreLoadingHandler}
+                className='w-[46rem] h-full font-semibold text-sm bg-myGray bg-opacity-30 rounded-md'>
+                더 보기
+              </button>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
