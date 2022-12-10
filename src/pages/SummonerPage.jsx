18d81cb84785c379e5d61987d2fbacca74ac3f06
k-kbk <kkbk-@naver.com>
2022-12-10 14:14:29 +0900

    feat: 맨 위로 이동 기능 구현

--- src/pages/SummonerPage.jsx
+++ src/pages/SummonerPage.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Nav from '../components/Nav';
 import SummonerInfo from '../components/Summoner/SummonerInfo';
 import MatchHistory from '../components/Match/MatchHistory';
 
 const SummonerPage = () => {
+  const { state: summoner } = useLocation();
+
+  const goTop = () => {
+    window.scrollTo({ top: 0 });
+  };
+
   return (
     <>
       <Nav />
-      <main className='max-w-4xl w-full h-auto flex flex-col justify-center items-center'>
+      <main className='w-full h-auto flex flex-col justify-center items-center'>
         <SummonerInfo />
-        <MatchHistory />
+        <MatchHistory summoner={summoner} />
+        <button
+          onClick={goTop}
+          className='font-semibold text-sm text-white w-auto h-auto fixed bottom-8 right-4 sm:right-10 px-2 py-2 bg-gray-300 sm:bg-none rounded-full shadow-md sm:shadow-none'>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/up.png`}
+            alt='up'
+            className='w-5 h-5 sm:w-8 sm:h-8'
+          />
+        </button>
       </main>
     </>
   );
