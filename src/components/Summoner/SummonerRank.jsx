df90bc24cc66de2a6d2b03fb6682f7a1a9ccb9f6
k-kbk <kkbk-@naver.com>
2022-12-10 14:15:56 +0900

    refactor: axios 요청 실패 시, 재요청

--- src/components/Summoner/SummonerRank.jsx
+++ src/components/Summoner/SummonerRank.jsx
@@ -1,11 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { memo, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import axios from 'axios';
+import Axios from 'axios';
 import Tier from './Tier';
+
+const axios = Axios.create();
+axios.interceptors.response.use(undefined, (error) => {
+  const config = error.config;
+  config.retryCount = isNaN(config.retryCount) ? 0 : config.retryCount;
+  config.retryCount += 1;
+  if (config.retryCount > 2) {
+    return Promise.reject(error);
+  }
+  return axios.request(config);
+});
 
 const SummonerRank = memo((props) => {
   const { state: summoner } = useLocation();
+  const [loading, setLoading] = useState(true);
   const [rankedSolo, setRankedSolo] = useState(null);
   const [rankedFlex, setRankedFlex] = useState(null);
   const API_KEY = process.env.REACT_APP_API_KEY;
@@ -22,19 +34,25 @@ const SummonerRank = memo((props) => {
           }
         )
         .then((res) => {
-          console.log(res);
           setRankedSolo(res.data[0]);
           setRankedFlex(res.data[1]);
+          setLoading(false);
         });
     };
     getLeagueData();
   }, [summoner]);
 
   return (
-    <div className='flex flex-col sm:flex-row justify-center items-center pt-5 pb-2'>
-      {rankedSolo && <Tier data={rankedSolo} />}
-      {rankedFlex && <Tier data={rankedFlex} />}
-    </div>
+    <>
+      {loading ? (
+        <div className='h-56 sm:h-36 flex justify-center items-center bg-gray-100'></div>
+      ) : (
+        <div className='flex flex-col sm:flex-row justify-center items-center pt-3 pb-5'>
+          {rankedSolo && <Tier data={rankedSolo} />}
+          {rankedFlex && <Tier data={rankedFlex} />}
+        </div>
+      )}
+    </>
   );
 });
 
